da2f4806e171014b52f3f4233c6535110e1619b4
Fredy <inztockz@gmail.com>
2025-07-11 17:11:24 -0500

    Actualizando

--- src/main/java/com/farmacia/repository/ProductoRepository.java
+++ src/main/java/com/farmacia/repository/ProductoRepository.java
@@ -2,6 +2,14 @@ package com.farmacia.repository;
 
 import com.farmacia.entity.ProductoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
+
+    @Query(" SELECT p " +
+            "FROM ProductoEntity p " +
+            "WHERE p.estado = true")
+    List<ProductoEntity> findAll();
 }
